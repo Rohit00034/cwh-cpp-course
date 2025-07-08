663c1507686bea2396df32d6692ed0bb9ee7bb52
Rohit00034 <rohitsahadev2506@gmail.com>
2025-07-08 22:01:13 +0530

    Create tut9and10ctrflow.cpp
    TOPIC:- CONTROL FLOW STATEMENTS,USE OF LOGICAL OPERATORS

---
+++ tut9and10ctrflow.cpp
@@ -0,0 +1,14 @@
+//we leaartn about control flow statements such as switch ,if ,else, for while (sequential flow, selective flow,loop flow)
+#include <iostream>
+using namespace std;
+int main(){
+ float gpa=7.88;
+    if(gpa<8.5 and gpa>7.5 ){
+        cout<<"avg";
+    }
+    return 0;
+    int age;
+// switch(age){
+    // case(age<10)    we cannot put conditions in the case statements 
+// } 
+}
