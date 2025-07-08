b903b18d1a44b89d2f57415313267099ae55d587
Rohit00034 <rohitsahadev2506@gmail.com>
2025-07-08 22:00:25 +0530

    Create tut8iomanpconst.cpp
    TOPICS:- Iomanip,const keyword,operator precedence

---
+++ tut8iomanpconst.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+    //use of const keyword
+    int a=22,b=1213;
+    //manipulators (endl,\n,setw etc.) in c++
+    cout<<"The value of a is "<<setw(4)<<a<<endl;
+    cout<<"The value of b is "<<setw(4)<<b<<endl;
+    //operator precedence(priority)and associativity(L to R/R to L)  check it on cppreference sites 
+    
+    return 0;
+}
