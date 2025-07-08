4f0ba903bbe3aaf069d2a11db3d2b80c9495af02
Rohit00034 <rohitsahadev2506@gmail.com>
2025-07-08 22:01:50 +0530

    Create tut11sniptsbrkcont.cpp
    TOPICS:- CODE SNIPPETS IN VSCODE,BOILERPLATE CODE

---
+++ tut11sniptsbrkcont.cpp
@@ -0,0 +1,10 @@
+/*learnt about break and continue statements, user defined snippets in vscode ,define them  and to use snippets as we want
+in our case we made code snippet begingeneralcode*/
+//we get this from begincodegeneral snippet :-
+// #include <iostream>
+// using namespace std;
+// int main(){
+    //|
+//     return 0;
+// }
+//THESE CODES WHICH ARE REPETITIVE WITH LESS TO NO CHANGES ARE CALLED BOILERPLATE CODES
