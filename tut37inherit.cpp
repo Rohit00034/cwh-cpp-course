fafc07be191e18b87bfbef7c785dad6b70578256
Rohit00034 <rohitsahadev2506@gmail.com>
2025-07-19 21:10:11 +0530

    Create tut37inherit.cpp
    LEARNT ABOUT INHERITANCE AND ITS SYNTAX WITH CONCEPTS

---
+++ tut37inherit.cpp
@@ -0,0 +1,42 @@
+//LEARNT ABOUT INHERITANCE AND ITS SYNTAX WITH CONCEPTS
+#include <iostream>
+using namespace std;
+class Employee{
+    int salary;
+    public:
+    int id;
+    Employee(int empid){
+id=empid;
+}
+Employee(){
+    cout<<"Default constructor employee called: ";
+}
+};
+class Programmer : Employee{
+public:
+    int langaugecode=9;
+Programmer(int empid){  /*as we have inherited from employee class whenever we will
+    create an object of programmer class the default constructor of employee class will 
+    be called first then parameterized constructor of the programmer class will be called*/
+cout<<"\nDefault constructor of Employee class called\n";
+    id = empid;
+}
+};
+/*Syntax for inheriting class
+class {{Derived class name}} : {{visibility mode}} {{Base class name}}{
+//members/methods etc.
+};*/
+/*NOTE:THE DEFAULT VISIBILITY MODE IS PRIVATE
+1)if teh visibility mode is private then PUBLIC MEMBERS of base class in the derived class 
+are inherited as private 
+2)if the visibility mode is public then PUBLIC MEMBERS of base class in the derived class 
+are inherited as public 
+3)PRIVATE MEMBERS OF A CLASS CAN NEVER BE INHERITED*/ 
+int main(){
+    Employee rohan(01);
+    cout<<rohan.id<<endl;
+    Programmer sudeep(02);
+// cout<<sudeep.id; can't id here as it was inherited as public
+cout<<sudeep.langaugecode;
+    return 0;
+}
