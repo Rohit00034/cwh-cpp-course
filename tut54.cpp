238c4e6d788a5f5d5cbebce7ad3b70b8e3a274b3
Rohit00034 <rohitsahadev2506@gmail.com>
2025-08-31 19:28:20 +0530

    Create tut54.cpp
    LEARNT THEORY ABOUT POLYMORPHISM AND ITS TYPES

---
+++ tut54.cpp
@@ -0,0 +1,19 @@
+//LEARNT THEORY ABOUT POLYMORPHISM AND ITS TYPES
+#include <iostream>
+using namespace std;
+int main(){
+    //polymorphism -means many forms
+    /*one name many forms
+    examples: function overloading,operator overloading,virtual functions
+    types:Compile time(uses early binding/static binding) and run time polymorphism
+    */
+   /*Polymorphism in C++ can be of two types:
+    1. Compile time polymorphism
+    Compile time polymorphism in C++ is acheived using:
+        1.1 Function overloading
+        1.2 Operator Overloading
+    2. Run time polymorphism
+    Run time polymorphism in C++ is acheived using:
+        2.1 Virtual functions*/
+    return 0;
+}
